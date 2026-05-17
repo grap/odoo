76a39117f649d51ef91b5126b8d25b5b78e5b67d
Sylvain LE GAL <sylvain.legal@grap.coop>
2026-05-17 21:07:16 +0200

    [CUSTOM] GRAP: account: disable feature that return account by supplier

--- addons/account/models/account_account.py
+++ addons/account/models/account_account.py
@@ -551,9 +551,12 @@ class AccountAccount(models.Model):
 
     @api.model
     def _name_search(self, name, args=None, operator='ilike', limit=100, name_get_uid=None):
-        if not name and self._context.get('partner_id') and self._context.get('move_type'):
-            return self._order_accounts_by_frequency_for_partner(
-                            self.env.company.id, self._context.get('partner_id'), self._context.get('move_type'))
+        # <GRAP>
+        # Disable weird feature that hide all possibilities if name is False
+        # if not name and self._context.get('partner_id') and self._context.get('move_type'):
+        #     return self._order_accounts_by_frequency_for_partner(
+        #                     self.env.company.id, self._context.get('partner_id'), self._context.get('move_type'))
+        # </GRAP>
         args = args or []
         domain = []
         if name:
